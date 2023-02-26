5119a837a5e6d6e9afef3b0a4c597cf3769ca477
Raul Pham <phatpham314@icloud.com>
2023-02-25 18:37:37 -0600

    Add Ingredients class and var highlights: [String] to Recipe.swift

--- Recipe List App/Models/Recipe.swift
+++ Recipe List App/Models/Recipe.swift
@@ -8,17 +8,25 @@
 import Foundation
 
 class Recipe: Identifiable, Decodable {
-    
-    var id:UUID?
-    var name:String
-    var featured:Bool
-    var image:String
-    var description:String
-    var prepTime:String
-    var cookTime:String
-    var totalTime:String
-    var servings:Int
-    var ingredients:[String]
-    var directions:[String]
-    
+      
+      var id:UUID?
+      var name:String
+      var featured:Bool
+      var image:String
+      var description:String
+      var prepTime:String
+      var cookTime:String
+      var totalTime:String
+      var servings:Int
+      var highlights: [String]
+      var ingredients:[Ingredients]
+      var directions:[String]
+}
+
+class Ingredients: Identifiable, Decodable {
+      var id: UUID?
+      var name: String
+      var num: Int?
+      var denom: Int?
+      var unit: Int?
 }
